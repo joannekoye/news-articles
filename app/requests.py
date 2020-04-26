4384154f4c0e5deac758f3ce769c23e1158f6c4b
Joan Nekoye <joannekoye@gmail.com>
2020-04-26 19:09:09 +0300

    add css styling and google fonts

--- app/requests.py
+++ app/requests.py
@@ -2,6 +2,7 @@ from app import app
 import urllib.request, json
 from .models import source, article
 
+
 Source = source.Source
 Article = article.Article
 
@@ -55,7 +56,32 @@ def get_sources():
 
     return news_sources
 
+def open_source_articles(source_id):
+    '''
+    Function that processes a news source and transforms it into an object list
 
+    Args:
+        source_id: An id of a specific news source
+
+    Returns:
+        source_articles: A list of items in the source object
+    
+    '''
+    get_news_source_url = everything_url.format(source_id, api_key)
+
+    with urllib.request.urlopen(get_news_source_url) as url:
+        source_data = url.read()
+        source_response = json.loads(source_data)
+
+        source_items = None
+
+        if source_response['articles']:
+            source_article_list = source_response['articles']
+            source_articles = process_articles(source_article_list)
+
+    return source_articles
+
+    
 def get_by_category(category):
     '''
     Function that gets the json response to our url request
@@ -146,30 +172,7 @@ def process_sources(source_list):
     return source_results
 
 
-def open_source_articles(source_id):
-    '''
-    Function that processes a news source and transforms it into an object list
 
-    Args:
-        source_id: An id of a specific news source
-
-    Returns:
-        source_articles: A list of items in the source object
-    
-    '''
-    get_news_source_url = everything_url.format(source_id, api_key)
-
-    with urllib.request.urlopen(get_news_source_url) as url:
-        source_data = url.read()
-        source_response = json.loads(source_data)
-
-        source_items = None
-
-        if source_response['articles']:
-            source_article_list = source_response['articles']
-            source_articles = process_articles(source_article_list)
-
-    return source_articles
 
 
 # def search_article(name , source_id):
